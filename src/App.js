1346b34b3ef0354d859f3cd6e1675f12b5dd50cb
Suman Saurabh <77678211+sumansrb@users.noreply.github.com>
2021-05-04 20:29:40 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 import './App.css'
 
 
-function Todo({ todo, index ,completeTodo, removeTodo}) {
+function Todo({ todo, index ,completeTodo, undoTodo, removeTodo}) {
   return( 
   <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} 
   className="todo">
   {todo.text}
   <div>
-    <button onClick={()=>completeTodo(index)}>Complete</button>
+    <button onClick={()=>completeTodo(index)}>Done</button>
+    <button onClick={()=>undoTodo(index)}>Undone</button>
     <button onClick={()=>removeTodo(index)}>x</button>
     
   </div>
@@ -20,7 +21,7 @@ function TodoForm({addTodo}) {
   const [value, setValue]= useState();
 
   const handleSubmit = e => {
-    e.preventDefault();
+    e.preventDefault(); 
     if (!value) return;
     addTodo(value);
     setValue('');
@@ -47,18 +48,28 @@ function App() {
   const addTodo = text => {
     const newTodos =[...todos, { text }];
     setTodos(newTodos); 
+    console.log('todo added')
   }
 
   const completeTodo = index => {
     const newTodos = [...todos];
     newTodos[index].isCompleted = true;
     setTodos(newTodos)
+    console.log('completed')
+  }
+
+  const undoTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = false;
+    setTodos(newTodos)
+    console.log('not yet completed')
   }
 
 const removeTodo= index => {
   const newTodos = [...todos];
   newTodos.splice(index, 1);
   setTodos(newTodos);
+  console.log('one todo removed')
 }
 
   const myStyle = {
@@ -87,6 +98,7 @@ const removeTodo= index => {
           index={index} 
           todo={todo}
           completeTodo={completeTodo}
+          undoTodo={undoTodo}
           removeTodo={removeTodo}
           />
         ))}
